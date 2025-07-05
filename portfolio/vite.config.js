a22c0929799e821100dd6c54b72b118a347fe3fe
Tiffany <yungfen8911@gmail.com>
2025-07-05 13:08:27 +0800

    Add GitHub Pages deployment config

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // 用戶頁面用根路徑
+  build: {
+    outDir: 'dist'
+  }
 })
