bcc7c770a5891077da8e263d566e08c4c0c2a2b1
Tiffany <yungfen8911@gmail.com>
2025-07-05 13:00:19 +0800

    Initial commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import NavBar from './components/NavBar.jsx'
+
+function App() {
+
+  return (
+    <>
+    <div className="App">
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a simple portfolio application.</p>
+    </div>
+      <NavBar />
+    </>
+  )
+}
+
+export default App
---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+    }
+
+export default NavBar;
